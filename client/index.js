13540620d5abf47734ca06ca13e861f1fc0ff6fc
polatengin <enpolat@microsoft.com>
2021-01-22 16:49:35 +0300

    default values for the api interaction

--- client/index.js
+++ client/index.js
@@ -6,3 +6,7 @@ const quantityElement = document.getElementById("quantity");
 
 const defaultColor = "#e4e4e4";
 const fillColor = "#2064aa";
+
+const PRODUCT_COUNT = 10;
+const PRODUCT_SHOW_DURATION = 15 * 1000; // in seconds
+
