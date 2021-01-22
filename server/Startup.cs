bae33b580cd462ce1a31dd6994e15117a8bf1c0b
polatengin <enpolat@microsoft.com>
2021-01-22 15:42:10 +0300

    server project created

---
+++ server/Startup.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace server
+{
+  public class Startup
+  {
+    public void ConfigureServices(IServiceCollection services)
+    {
+    }
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+    }
+  }
+}
