d23375e249050193640eb2eb1ae384bb013ee806
polatengin <enpolat@microsoft.com>
2021-01-22 16:53:20 +0300

    get data from api

--- client/index.js
+++ client/index.js
@@ -10,6 +10,15 @@ const fillColor = "#2064aa";
 const PRODUCT_COUNT = 10;
 const PRODUCT_SHOW_DURATION = 15 * 1000; // in seconds
 
+let data = [];
+let currentIndex = 0;
+
+const fetchData = async () => {
+  // fetch latest product sale data from API
+  data = await (await fetch("http://localhost:5000")).json();
+
+
+};
 
 const showNextData = async () => {
   // get current sale info from fetched data
