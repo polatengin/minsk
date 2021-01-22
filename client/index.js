ddf752c3aa08f27ccecfac94c6457f2d3af0b96f
polatengin <enpolat@microsoft.com>
2021-01-22 16:46:54 +0300

    color codes for city pieces of the turkish map

--- client/index.js
+++ client/index.js
@@ -3,3 +3,6 @@ const mapElement = document.getElementById("map");
 const cityNameElement = document.getElementById("cityName");
 const productNameElement = document.getElementById("productName");
 const quantityElement = document.getElementById("quantity");
+
+const defaultColor = "#e4e4e4";
+const fillColor = "#2064aa";
