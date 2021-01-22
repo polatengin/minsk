4ff53e0236d00a4b0702bb1abffb7da685284d22
polatengin <enpolat@microsoft.com>
2021-01-22 16:45:39 +0300

    get html elements to display current sale data

--- client/index.js
+++ client/index.js
@@ -1,2 +1,5 @@
 const mapElement = document.getElementById("map");
 
+const cityNameElement = document.getElementById("cityName");
+const productNameElement = document.getElementById("productName");
+const quantityElement = document.getElementById("quantity");
